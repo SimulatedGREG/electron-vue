7d6c94eb210393c6f71a9a5c8ec5ffc68fffb733
sqlwwx <wwx_2012@hotmail.com>
2016-06-01 23:47:35 +0800

    add sample for notify

---
+++ app/src/components/HomeView/Natives.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="$root.notify({title: 'electron', body: 'hello', onActivate: ()=> {test()}})">notify</button>
+</template>
+<script>
+  export default {
+    data () {
+      return {count: 1}
+    },
+    methods: {
+      test () {
+        console.log(this.count++)
+      }
+    }
+  }
+</script>
