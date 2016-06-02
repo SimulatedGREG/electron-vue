bc9c8a59b089b18a57daaec50c0c77b565f44718
SimulatedGREG <simulatedgreg@gmail.com>
2016-06-02 11:06:27 -0500

    add vue-electron

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import Electron from 'vue-electron'
 import Resource from 'vue-resource'
 import Router from 'vue-router'
 
 import App from './App'
 import routes from './routes'
 
+Vue.use(Electron)
 Vue.use(Resource)
 Vue.use(Router)
 Vue.config.debug = true
