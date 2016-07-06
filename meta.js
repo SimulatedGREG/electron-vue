7acdf3ca2119b8ef6ac61018d1a4988dbe890265
SimulatedGREG <simulatedgreg@gmail.com>
2016-07-06 03:31:20 -0500

    update completeMessage

--- meta.js
+++ meta.js
@@ -16,7 +16,7 @@ module.exports = {
     eslint: {
       type: 'confirm',
       require: true,
-      message: 'Use ESLint (extends `standard`)',
+      message: 'Enable ESLint (extends `standard`)?',
       default: true
     },
     plugins: {
@@ -58,6 +58,14 @@ module.exports = {
     'tasks/vue/**/*': 'plugins[\'vue-router\']',
     'tasks/vuex/**/*': 'plugins[\'vuex\']'
   },
-  completeMessage: `All set. More configurations can be made at \x1b[33mconfig.js\x1b[0m.
-    `
+  completeMessage: `---
+
+All set. More configurations can be made at \x1b[33m{{destDirName}}/config.js\x1b[0m.
+
+Make sure to check out the documentation for this boilerplate at \x1b[33mhttps://simulatedgreg.gitbooks.io/electron-vue/content/index.html\x1b[0m.
+
+Next steps:
+  1. \x1b[34mcd {{destDirName}}\x1b[0m
+  2. \x1b[34mnpm install\x1b[0m
+  3. \x1b[34mnpm run dev\x1b[0m`
 }
