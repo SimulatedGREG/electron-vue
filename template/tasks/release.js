9549588731b0c544ba8cf1482ca9fce7acf6186c
SimulatedGREG <simulatedgreg@gmail.com>
2016-08-06 13:51:58 -0500

    update error handling on electron-packager issues

--- template/tasks/release.js
+++ template/tasks/release.js
@@ -26,11 +26,14 @@ function build () {
 
   console.log('\x1b[34mBuilding electron app(s)...\n\x1b[0m')
   packager(options, (err, appPaths) => {
-    if(err) console.error(err)
+    if(err) {
+      console.error('\x1b[31mError from `electron-packager` when building app...\x1b[0m')
+      console.error(err)
+    } else {
+      console.log('Build(s) successful!')
+      console.log(appPaths)
 
-    console.log('Build(s) successful!')
-    console.log(appPaths)
-
-    console.log('\n\x1b[34mDONE\n\x1b[0m')
+      console.log('\n\x1b[34mDONE\n\x1b[0m')
+    }
   })
 }
