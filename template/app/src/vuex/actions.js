4f29c8995e653ca0658bd4f9635ae545e55d1b35
李大双 <ldshuang@gmail.com>
2016-08-23 18:03:49 +0800

    fix typo

--- template/app/src/vuex/actions.js
+++ template/app/src/vuex/actions.js
@@ -1,9 +1,9 @@
 import * as types from './mutation-types'
 
 export const decrementMain = ({ {{#if_eq vueVersion 'next'}}commit{{else}}dispatch{{/if_eq}} }) => {
-  {{#if_eq vueVersion 'next'}}commit{{else}}dispatch{{/if_eq}}(types.DECREAMENT_MAIN_COUNTER)
+  {{#if_eq vueVersion 'next'}}commit{{else}}dispatch{{/if_eq}}(types.DECREMENT_MAIN_COUNTER)
 }
 
 export const incrementMain = ({ {{#if_eq vueVersion 'next'}}commit{{else}}dispatch{{/if_eq}} }) => {
-  {{#if_eq vueVersion 'next'}}commit{{else}}dispatch{{/if_eq}}(types.INCREAMENT_MAIN_COUNTER)
+  {{#if_eq vueVersion 'next'}}commit{{else}}dispatch{{/if_eq}}(types.INCREMENT_MAIN_COUNTER)
 }
