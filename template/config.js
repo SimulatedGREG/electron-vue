d889c8b1de285a073e66b0dbdfa552f64747438f
Mark Lee <electronjs@lazymalevolence.com>
2016-08-28 15:23:27 -0700

    Update for Electron Packager 8.0.0

--- template/config.js
+++ template/config.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const path = require('path')
-const pkg = require('./app/package.json')
 
 let config = {
   // Name of electron app
@@ -20,7 +19,6 @@ let config = {
   // electron-packager options
   // Docs: https://simulatedgreg.gitbooks.io/electron-vue/content/docs/building_your_app.html
   building: {
-    'app-version': pkg.version,
     arch: 'x64',
     asar: true,
     dir: path.join(__dirname, 'app'),
