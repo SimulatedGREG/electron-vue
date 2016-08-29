378355a5f1a4567a6dc49a4708446bd6a2580f86
SimulatedGREG <simulatedgreg@gmail.com>
2016-08-29 03:11:29 -0500

    initial setup

--- tests/scaffold.js
+++ tests/scaffold.js
@@ -1,19 +1,50 @@
 'use strict'
 
+const { spawn } = require('child_process')
 const suppose = require('suppose')
+const builds = require('./builds.json')
+
+let YELLOW = '\x1b[33m'
+let BLUE = '\x1b[34m'
+let END = '\x1b[0m'
+
 process.chdir(process.cwd() + '/tests/builds')
 
-suppose('vue', ['init', 'simulatedgreg/electron-vue', 'm'], {debug: process.stdout})
-  .when(/Application Name/g).respond('\n')
-  .when(/Project description/g).respond('\n')
-  .when(/version/g).respond('\u001b[B\n')
-  .when(/ESLint/g).respond('\n')
-  .when(/config/g).respond('\n')
-  .when(/plugins/g).respond('\n')
-  .when(/author/g).respond('\n')
-.on('error', err => {
-  console.log(err.message)
+Object.keys(builds).forEach(key => {
+  generate(key, builds[key])
 })
-.end(code => {
-  console.log('done', code)
-})
+
+function generate (key, build) {
+  console.log(`${YELLOW}Generate: ${key}${END}`)
+  suppose('vue', ['init', 'simulatedgreg/electron-vue', key], { debug: process.stdout })
+    .when(/Application Name/g).respond(build[0])
+    .when(/Project description/g).respond(build[1])
+    .when(/version/g).respond(build[2])
+    .when(/ESLint/g).respond(build[3])
+    .when(/config/g).respond(build[4])
+    .when(/plugins/g).respond(build[5])
+    .when(/author/g).respond(build[6])
+  .on('error', err => {
+    console.log(err.message)
+  })
+  .end(code => {
+    if (code === 0) install(key)
+  })
+}
+
+function install (key) {
+  let child = spawn('npm', ['install', '--depth', '0'], { cwd: `${process.cwd()}/${key}`, stdio: 'inherit' })
+
+  child.on('close', code => {
+    if (code === 0) pack(key)
+  })
+}
+
+function pack (key) {
+  let child = spawn('npm', ['run', 'pack'], { cwd: `${process.cwd()}/${key}`, stdio: 'inherit' })
+
+  child.on('close', code => {
+    console.log(`${BLUE}\`${key}\` passed${END}`)
+  })
+}
+
