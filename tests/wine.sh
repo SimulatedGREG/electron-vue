0d89e8d78448a9f9eedd9f7e3176856e8a5a7f4c
SimulatedGREG <simulatedgreg@gmail.com>
2016-09-07 02:19:58 -0500

    add wine install for ci

---
+++ tests/wine.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update lists
+sudo apt-get update
+
+# Preset agreement to install ttf programmatically
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+sudo apt-get -y install ttf-mscorefonts-installer
+
+# Install wine for win32 building support
+sudo apt-get -y install wine
