d137cd32e9b864c5963b0717d9ec4b2ccf5c5d93
SimulatedGREG <simulatedgreg@gmail.com>
2016-09-21 06:12:27 -0500

    update spec to vue@next support

--- template/test/unit/specs/LandingPageView.spec.js
+++ template/test/unit/specs/LandingPageView.spec.js
@@ -4,14 +4,18 @@ import LandingPageView from 'src/components/LandingPageView'
 describe('LandingPageView.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
+      {{#if_eq vueVersion 'next'}}
+      el: document.createElement('div'),
+      render: h => h(LandingPageView)
+      {{else}}
       components: { LandingPageView },
       template: `
         <div>
           <landing-page-view></landing-page-view>
         </div>
       `
+      {{/if_eq}}
     }).$mount()
-
     expect(vm.$el.querySelector('h1').textContent).toContain('Welcome.')
   })
 })
