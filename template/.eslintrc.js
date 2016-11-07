b20953c24d8bab0a70878f9be5743bb18d923706
SimulatedGREG <simulatedgreg@gmail.com>
2016-11-07 03:18:25 -0600

    use `babel-eslint` for eslint parser (#59), update .gitignore

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
+    sourceType: 'module'
   },
   env: {
     browser: true,
