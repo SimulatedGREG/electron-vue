873a03dee89d815238ab0da1b894c1b9af94eb24
SimulatedGREG <simulatedgreg@gmail.com>
2016-12-05 14:34:12 -0600

    :bug: fix unit testing when trying to evaluate

--- template/app/src/components/LandingPageView/CurrentPage.vue
+++ template/app/src/components/LandingPageView/CurrentPage.vue
@@ -17,3 +17,17 @@
     You are currently at <code>`\{{ $route.path }}`</code> on the <code>`\{{ $route.name }}`</code> view.
   </p>
 </template>
+
+<script>
+  export default {
+    created () {
+      // Set $route values that are not preset during unit testing
+      if (process.env.NODE_ENV === 'testing') {
+        this.$route = {
+          name: 'landing-page',
+          path: '/landing-page'
+        }
+      }
+    }
+  }
+</script>
