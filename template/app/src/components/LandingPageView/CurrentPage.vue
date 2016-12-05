9e9ffb95453e6b726c8a6b038a49c79228735cb2
SimulatedGREG <simulatedgreg@gmail.com>
2016-12-05 15:14:43 -0600

    :bug: fix airbnb eslint issues

--- template/app/src/components/LandingPageView/CurrentPage.vue
+++ template/app/src/components/LandingPageView/CurrentPage.vue
@@ -20,14 +20,14 @@
 
 <script>
   export default {
-    created () {
+    created{{#if_eq eslintConfig 'standard'}} {{/if_eq}}() {
       // Set $route values that are not preset during unit testing
       if (process.env.NODE_ENV === 'testing') {
         this.$route = {
           name: 'landing-page',
-          path: '/landing-page'
-        }
+          path: '/landing-page'{{#if_eq eslintConfig 'airbnb'}},{{/if_eq}}
+        }{{#if_eq eslintConfig 'airbnb'}};{{/if_eq}}
       }
-    }
-  }
+    }{{#if_eq eslintConfig 'airbnb'}},{{/if_eq}}
+  }{{#if_eq eslintConfig 'airbnb'}};{{/if_eq}}
 </script>
