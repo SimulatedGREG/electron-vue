e0c2eb071da152b18c6e94cb4b7058ddec1df0de
SimulatedGREG <simulatedgreg@gmail.com>
2017-01-20 04:06:19 -0600

    :rotating_light: fix airbnb-base lint warning

--- template/app/src/main/index.dev.js
+++ template/app/src/main/index.dev.js
@@ -20,7 +20,7 @@ require('electron-debug')({ showDevTools: true })
 require('electron').app.on('ready', () => {
   let installExtension = require('electron-devtools-installer')
   installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(name => {})
+    .then(() => {})
     .catch(err => {
       console.log('Unable to install `vue-devtools`: \n', err)
     })
