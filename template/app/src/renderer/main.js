2f97c0a00ec3393a0894309d39ce9051abdad203
SimulatedGREG <simulatedgreg@gmail.com>
2017-01-20 03:08:18 -0600

    :bug: fix `renderer/main.js` templating

--- template/app/src/renderer/main.js
+++ template/app/src/renderer/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
+{{#isEnabled plugins 'vue-electron'}}
 import Electron from 'vue-electron'
+{{/isEnabled}}
+{{#isEnabled plugins 'vue-resource'}}
 import Resource from 'vue-resource'
+{{/isEnabled}}
+{{#isEnabled plugins 'vue-router'}}
 import Router from 'vue-router'
 
 import App from './App'
 import routes from './routes'
+{{/isEnabled}}
 
+{{#isEnabled plugins 'vue-electron'}}
 Vue.use(Electron)
+{{/isEnabled}}
+{{#isEnabled plugins 'vue-resource'}}
 Vue.use(Resource)
+{{/isEnabled}}
+{{#isEnabled plugins 'vue-router'}}
 Vue.use(Router)
+{{/isEnabled}}
 Vue.config.debug = true
 
+{{#isEnabled plugins 'vue-router'}}
 const router = new Router({
   scrollBehavior: () => ({ y: 0 }),
   routes
@@ -21,3 +34,11 @@ new Vue({
   router,
   ...App
 }).$mount('#app')
+{{else}}
+import App from './App'
+
+/* eslint-disable no-new */
+new Vue({
+  ...App
+}).$mount('#app')
+{{/isEnabled}}
