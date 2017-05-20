a5b2a303db4b76a0d35dc8e4583a020bb821cb1d
Greg Holguin <simulatedgreg@gmail.com>
2017-05-20 03:19:58 -0500

    update style of auto updater comments

--- template/src/main/index.js
+++ template/src/main/index.js
@@ -39,20 +39,22 @@ app.on('activate', () => {
 })
 {{#if_eq builder 'bulider'}}
 
-//-------------------------------------------------------------------------
-// Auto Updater
-//
-// Uncomment the following code below and install `electron-updater` to
-// support auto updating. Code Signing with valid certificates is REQUIRED.
-//-------------------------------------------------------------------------
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ */
 
-// import { autoUpdater } from 'electron-updater'
+/*
+import { autoUpdater } from 'electron-updater'
 
-// autoUpdater.on('update-downloaded', () => {
-//   autoUpdater.quitAndInstall()
-// })
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
 
-// app.on('ready', () => {
-//   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-// })
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
 {{/if_eq}}
