c6219b1a81d319d5767ca0ecb81c4298ec96a79d
SimulatedGREG <simulatedgreg@gmail.com>
2017-06-02 23:23:23 -0500

    stop build step when webpack produces errors

--- template/.electron-vue/build.js
+++ template/.electron-vue/build.js
@@ -56,15 +56,21 @@ function build () {
   pack(mainConfig).then(result => {
     results += result + '\n\n'
     m.success('main')
-  }).catch(() => {
+  }).catch(err => {
     m.error('main')
+    console.log(`\n  ${errorLog}failed to build main process`)
+    console.error(`\n${err}\n`)
+    process.exit(1)
   })
 
   pack(rendererConfig).then(result => {
     results += result + '\n\n'
     m.success('renderer')
-  }).catch(() => {
+  }).catch(err => {
     m.error('renderer')
+    console.log(`\n  ${errorLog}failed to build renderer process`)
+    console.error(`\n${err}\n`)
+    process.exit(1)
   })
 }
 
@@ -72,8 +78,20 @@ function pack (config) {
   return new Promise((resolve, reject) => {
     webpack(config, (err, stats) => {
       if (err) reject(err.stack || err)
-      else if (stats.hasErrors()) reject(stats.toJson().errors)
-      else {
+      else if (stats.hasErrors()) {
+        let err = ''
+
+        stats.toString({
+          chunks: false,
+          colors: true
+        })
+        .split(/\r?\n/)
+        .forEach(line => {
+          err += `    ${line}\n`
+        })
+
+        reject(err)
+      } else {
         resolve(stats.toString({
           chunks: false,
           colors: true
