93da84fb316e800b199fe2e4362ed58ced14f38c
snovey <isnovey@gmail.com>
2017-09-20 22:21:12 +0800

    Update vue-loader version

--- template/src/renderer/router/index.js
+++ template/src/renderer/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'landing-page',
-      component: require('@/components/LandingPage')
+      component: () => import('@/components/LandingPage')
     },
     {
       path: '*',
