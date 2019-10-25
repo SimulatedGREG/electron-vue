a4d1f0e5397ce704c223a0880a0a246e13f4ff64
Jiangx <994898372@qq.com>
2019-10-25 23:56:34 +0800

    fix：delete useless code
    Already in index.dev.js Quoted index.js. Should not add index.js to the entry file.

--- template/.electron-vue/dev-runner.js
+++ template/.electron-vue/dev-runner.js
@@ -79,7 +79,7 @@ function startRenderer () {
 
 function startMain () {
   return new Promise((resolve, reject) => {
-    mainConfig.entry.main = [path.join(__dirname, '../src/main/index.dev.js')].concat(mainConfig.entry.main)
+    mainConfig.entry.main = path.join(__dirname, '../src/main/index.dev.js')
     mainConfig.mode = 'development'
     const compiler = webpack(mainConfig)
 
