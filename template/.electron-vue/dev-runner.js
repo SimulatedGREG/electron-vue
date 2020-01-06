9aaf8b7f9f6ed1545507c5bd97d2f7de021c73b6
Egor <Egor3f@users.noreply.github.com>
2020-01-06 18:01:58 +0300

    Fix hot reload resetting whole renderer state
    From https://github.com/SimulatedGREG/electron-vue/issues/437

--- template/.electron-vue/dev-runner.js
+++ template/.electron-vue/dev-runner.js
@@ -64,8 +64,9 @@ function startRenderer () {
       {
         contentBase: path.join(__dirname, '../'),
         quiet: true,
+        hot: true,
         before (app, ctx) {
-          app.use(hotMiddleware)
+          // app.use(hotMiddleware)
           ctx.middleware.waitUntilValid(() => {
             resolve()
           })
