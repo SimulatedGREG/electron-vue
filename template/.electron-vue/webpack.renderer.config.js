8a77e3241e2a54ca00bb31a59191b134bf1c8338
tsflow <houhanbindingyan@163.com>
2021-01-12 15:45:56 +0800

    fix : process is not defined

--- template/.electron-vue/webpack.renderer.config.js
+++ template/.electron-vue/webpack.renderer.config.js
@@ -129,6 +129,18 @@ let rendererConfig = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, '../src/index.ejs'),
+      templateParameters(compilation, assets, options) {
+        return {
+          compilation: compilation,
+          webpack: compilation.getStats().toJson(),
+          webpackConfig: compilation.options,
+          htmlWebpackPlugin: {
+            files: assets,
+            options: options,
+          },
+          process,
+        };
+      },
       minify: {
         collapseWhitespace: true,
         removeAttributeQuotes: true,
